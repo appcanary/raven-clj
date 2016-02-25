be3b9305c3626db31c3aa9738cdde886187bdc2a
Max Veytsman <maxim@ontoillogical.com>
2016-02-25 16:18:19 -0500

    Fixed bug where we were sending extra parameters to sentry
    Updated protocol version

--- src/raven_clj/core.clj
+++ src/raven_clj/core.clj
@@ -14,18 +14,8 @@
           uri project-id))
 
 (defn make-sentry-header [ts key secret]
-  (format "Sentry sentry_version=2.0, sentry_client=raven-clj/0.6.0, sentry_timestamp=%s, sentry_key=%s, sentry_secret=%s"
+  (format "Sentry sentry_version=2.0, sentry_client=raven-clj/1.2.1, sentry_timestamp=%s, sentry_key=%s, sentry_secret=%s"
           ts key secret))
-
-(defn send-packet [{:keys [ts uri project-id key secret] :as packet-info}]
-  (let [url (make-sentry-url uri project-id)
-        header (make-sentry-header ts key secret)]
-    (http/post url
-               {:insecure? true
-                :throw-exceptions false
-                :headers {"X-Sentry-Auth" header
-                          "User-Agent" "raven-clj/0.6.0"}
-                :body (json/generate-string packet-info)})))
 
 (defn parse-dsn [dsn]
   (let [[proto-auth url] (string/split dsn #"@")
@@ -35,18 +25,27 @@
      :secret secret
      :uri (format "%s://%s" protocol
                   (string/join
-                    "/" (butlast (string/split url #"/"))))
+                   "/" (butlast (string/split url #"/"))))
      :project-id (Integer/parseInt (last (string/split url #"/")))}))
+
+(defn send-packet [dsn packet-info]
+  (let [{:keys [ts uri project-id key secret]} (parse-dsn dsn)
+        url (make-sentry-url uri project-id)
+        header (make-sentry-header ts key secret)]
+    (http/post url
+               {:throw-exceptions false
+                :headers {"X-Sentry-Auth" header
+                          "User-Agent" "raven-clj/1.3.1"}
+                :body (json/generate-string packet-info)})))
 
 (defn capture [dsn event-info]
   "Send a message to a Sentry server.
   event-info is a map that should contain a :message key and optional
   keys found at http://sentry.readthedocs.org/en/latest/developer/client/index.html#building-the-json-packet"
   (send-packet
-   (merge (parse-dsn dsn)
-          {:level "error"
+   dsn
+   (merge {:level "error"
            :platform "clojure"
-           :server_name (.getHostName (InetAddress/getLocalHost))
-           :ts (str (Timestamp. (.getTime (Date.))))}
+           :server_name (.getHostName (InetAddress/getLocalHost))}
           event-info
           {:event_id (generate-uuid)})))
